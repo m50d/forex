008ebdf2949b794a9ade08a8d747b8e6c48c4338
Michael Donaghy <md401@srcf.ucam.org>
2019-07-24 15:06:14 +0100

    Report full error to client

--- src/main/scala/forex/http/rates/RatesHttpRoutes.scala
+++ src/main/scala/forex/http/rates/RatesHttpRoutes.scala
@@ -4,12 +4,12 @@ package rates
 import cats.effect.Sync
 import cats.syntax.flatMap._
 import forex.programs.RatesProgram
-import forex.programs.rates.{ Protocol => RatesProgramProtocol }
+import forex.programs.rates.{Protocol => RatesProgramProtocol}
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
 
-class RatesHttpRoutes[F[_]: Sync](rates: RatesProgram[F]) extends Http4sDsl[F] {
+class RatesHttpRoutes[F[_] : Sync](rates: RatesProgram[F]) extends Http4sDsl[F] {
 
   import Converters._, QueryParams._, Protocol._
 
@@ -17,8 +17,9 @@ class RatesHttpRoutes[F[_]: Sync](rates: RatesProgram[F]) extends Http4sDsl[F] {
 
   private val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F] {
     case GET -> Root :? FromQueryParam(from) +& ToQueryParam(to) =>
-      rates.get(RatesProgramProtocol.GetRatesRequest(from, to)).flatMap(Sync[F].fromEither).flatMap { rate =>
+      rates.get(RatesProgramProtocol.GetRatesRequest(from, to)).flatMap { case Right(rate) =>
         Ok(rate.asGetApiResponse)
+      case Left(programError) => InternalServerError(programError.getMessage)
       }
   }
 
