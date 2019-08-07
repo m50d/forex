2ed238e225f611017e8af5620be5e3a981bc0ccc
Michael Donaghy <md401@srcf.ucam.org>
2019-08-07 14:04:43 +0100

    Confirm populator is functional in integration test

--- src/test/scala/forex/services/rates/populator/FetchQuotesIT.scala
+++ src/test/scala/forex/services/rates/populator/FetchQuotesIT.scala
@@ -2,16 +2,24 @@ package forex.services.rates.populator
 
 import cats.effect.{ExitCode, IO, IOApp}
 import forex.config.ApplicationConfig
+import forex.domain.Rate
+import forex.services.Caches
 import pureconfig.generic.auto._
+import scalacache.Cache
+import scalacache.CatsEffect.modes.async
 
 object FetchQuotesIT extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] = {
+    implicit val cache: Cache[Rate] = Caches.guavaRates
     val config = pureconfig.loadConfigOrThrow[ApplicationConfig]("app")
     val service = new OneForgeService[IO](config.oneforge)
+    val populator = new SchedulingPopulator[IO](service)
     for {
       results ← service.getAll
       _ ← IO.delay(println(results))
+      timeToDelay ← populator.stepOnce
+      _ ← IO.delay(println(timeToDelay))
     } yield ExitCode.Success
   }
 
